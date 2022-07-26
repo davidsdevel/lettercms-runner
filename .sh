3939d0b77ca58ffb526fd3fb0ed574eca729bf2c
davidsdevel <djgm1206@gmail.com>
2022-07-26 19:17:01 -0430

    Test sh deploy

---
+++ .sh
@@ -0,0 +1,17 @@
+export HTTP_PROXY="http://{$RUNNER_NAME}.herokuapp.com"
+export HTTPS_PROXY="https://{$RUNNER_NAME}.herokuapp.com"
+
+echo $HTTPS_PROXY
+
+gitlab-runner register \
+  --non-interactive \
+  --url "https://gitlab.com/" \
+  --registration-token "$GITLAB_TOKEN" \
+  --executor "docker" \
+  --docker-image alpine:latest \
+  --description "$RUNNER_NAME" \
+  --maintenance-note "Free-form maintainer notes about this runner" \
+  --tag-list "docker,aws" \
+  --run-untagged="true" \
+  --locked="false" \
+  --access-level="not_protected"
