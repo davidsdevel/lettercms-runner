3c2b627b3a172756ddec275bddc08b5904d31da7
davidsdevel <djgm1206@gmail.com>
2022-07-26 22:50:43 -0430

    Try docker

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-echo "$USER"
-whoami
-
-mkdir $BUILD_DIR/runner-bin
-mkdir $BUILD_DIR/gitlab-runner
-
-echo "Fetching runner"
-curl -L --output $BUILD_DIR/runner-bin/gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
-
-chmod +x $BUILD_DIR/runner-bin/gitlab-runner
-
-export  PATH=$BUILD_DIR/runner-bin
-
-sudo gitlab-runner install --user=$USER --working-directory=$BUILD_DIR/gitlab-runner
-sudo gitlab-runner start
+docker run --rm -t -i gitlab/gitlab-runner --help
