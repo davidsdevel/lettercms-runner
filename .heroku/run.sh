632872ec185ef8870be4ad2c9af642c68b625b31
davidsdevel <djgm1206@gmail.com>
2022-07-26 21:51:41 -0430

    Updates

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y useradd
+
 mkdir $BUILD_DIR/bin
+mkdir $BUILD_DIR/gitlab-runner
 
 echo "Fetching binaries"
 curl -L --output $BUILD_DIR/bin/gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
@@ -9,7 +13,8 @@ chmod +x $BUILD_DIR/bin/gitlab-runner
 
 export  PATH=$BUILD_DIR/bin
 
-mkdir $BUILD_DIR/gitlab-runner
+useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 
-gitlab-runner install --working-directory=$BUILD_DIR/gitlab-runner
+
+gitlab-runner install --user=gitlab-runner --working-directory=$BUILD_DIR/gitlab-runner
 gitlab-runner start
